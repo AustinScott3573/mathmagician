8aa3438c8368959e0834b26a2372851179af9546
tannert44 <tannert8155@gmail.com>
2015-10-23 15:31:46 -0500

    Even Test pasing

--- MathmagicianTests/EvenTest.cs
+++ MathmagicianTests/EvenTest.cs
@@ -13,5 +13,45 @@ namespace MathmagicianTests
             EvenNumbers even = new EvenNumbers();
             Assert.IsNotNull(even);
         }
+
+        [TestMethod]
+        public void ShouldPrintFirstNumber()
+        {
+            EvenNumbers even = new EvenNumbers();
+            int expected = 2;
+            int actual = even.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPrintNextNumber()
+        {
+            EvenNumbers even = new EvenNumbers();
+            int input = 2;
+            int expected = 4;
+            int actual = even.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrintOfSequenceOfNumbers()
+        {
+            EvenNumbers even = new EvenNumbers();
+            int length = 8;
+            string expected = "2 4 6 8 10 12 14 16";
+            string actual = even.ToString(even.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPrintAVariableSequenceOfNumbers()
+        {
+            EvenNumbers nats = new EvenNumbers();
+            int input = 8;
+            int[] expected = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
+            int[] actual = nats.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
     }
 }
